00c62248e635223b3cbaded1a07941be9dcb4344
ishibashi <e185714@ie.u-ryukyu.ac.jp>
2022-01-19 02:14:56 +0900

    first commit

---
+++ plotting.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
+import numpy as np
+
+refseq = [330, 352, 353, 363, 377, 379, 380, 381, 395, 420, 422, 423, 435, 437, 438, 481, 487, 488, 495, 520]
+alpha = [347, 348, 358, 372, 374, 375, 376, 390, 415, 417, 418, 430, 432, 433, 476, 482, 483, 484, 490, 495, 498]
+beta = [347, 348, 358, 372, 374, 375, 376, 390, 415, 417, 418, 430, 432, 433, 476, 481, 482, 483, 490, 495, 498]
+gamma = [350, 351, 361, 375, 377, 378, 379, 393, 418, 420, 421, 433, 435, 436, 479, 484, 485, 486, 493, 498, 501]
+delta = [348, 349, 359, 373, 375, 376, 377, 391, 416, 418, 419, 431, 433, 434, 447, 476, 477, 483, 484, 491]
+omicron = [334, 347, 348, 358, 371, 374, 375, 376, 390, 415, 417, 418, 430, 432, 470, 474, 475, 476, 482, 483, 494, 499]
+
+l = [refseq,alpha,beta,gamma,delta,omicron]
+l2 = []
+for row in l:
+    l2.append(pd.Series(row))
+
+label = ["refseq","alpha","beta","gamma","delta","omicron"]
+
+df = pd.DataFrame()
+for i in range(len(l2)):
+    df[label[i]] = l2[i]
+
+sns.stripplot(data = df,jitter=False,size=4)
+plt.show()
---
+++ rbd.py
@@ -0,0 +1,121 @@
+import re
+import csv
+import pickle
+
+
+# scsの出現回数を数える
+def count_scs(l):
+    d = {}
+    scs = 5
+    for sub in l:
+        for i in range(len(sub) - scs + 1):
+            if sub[i:i+scs] not in d.keys():
+                d[sub[i:i+scs]] = 1
+            else:
+                d[sub[i:i+scs]] += 1
+    
+    return d
+
+# とりあえず1つずつ
+def def_rbd(type):
+    if type == "refseq" or type == "gamma": # and gamma
+        start = 330
+        end = 521
+    elif type == "omicron" or type == "alpha" or type == "beta": # and alpha, beta
+        start = 327
+        end = 518
+    elif type == "delta":
+        start = 328
+        end = 519
+    
+    else:
+        return print("typeはrefseq,omicronまたはdeltaです")
+
+
+    return start, end
+
+
+def p_load():
+    with open("hdic.pickle","rb") as f:
+        hdic = pickle.load(f)
+
+
+def csv_nonself_count(path,type):
+    start,end = def_rbd(type)
+    rbd_length = end - start + 1 # 今後使うかも
+
+    all_list = []
+    all_rbd_list = []
+    nonself_num_list = []
+    rbd_nonself_list = []
+    nonself_place_list = []
+    rbd_nonself_place_list = []
+    with open(path,"r") as f:
+        read = csv.reader(f)
+        all_aa = ""
+        rbd_aa = ""
+        nonself_num = 0
+        rbd_nonself_num = 0
+        nonself_place = []
+        rbd_nonself_place = []
+        for row in read:
+            aa_num = row[0].split("-")
+
+            #ここでリセット
+            if int(aa_num[-1]) == 1:
+                all_list.append(all_aa)
+                all_rbd_list.append(rbd_aa)
+                nonself_num_list.append(nonself_num)
+                rbd_nonself_list.append(rbd_nonself_num)
+                nonself_place_list.append(nonself_place)
+                rbd_nonself_place_list.append(rbd_nonself_place)
+
+                all_aa = ""
+                rbd_aa = ""
+                nonself_num = 0
+                rbd_nonself_num = 0
+                nonself_place = []
+                rbd_nonself_place = []
+
+            all_aa += row[2]
+            
+
+            if start <= int(aa_num[-1]) < end:
+                if int(row[1]) == 1:
+                    rbd_nonself_num += 1
+                    rbd_nonself_place.append(int(aa_num[-1]))
+
+                rbd_aa += row[2]
+            else:
+                if row[1].isdecimal() == True:
+                    if int(row[1]) == 1:
+                        nonself_num += 1
+                        nonself_place.append(int(aa_num[-1]))
+
+        
+        all_list.append(all_aa)
+        all_rbd_list.append(rbd_aa)
+        nonself_num_list.append(nonself_num)
+        rbd_nonself_list.append(rbd_nonself_num)
+        nonself_place_list.append(nonself_place)
+        rbd_nonself_place_list.append(rbd_nonself_place)
+
+        all_list.pop(0)
+        all_rbd_list.pop(0)
+        nonself_num_list.pop(0)
+        rbd_nonself_list.pop(0)
+        nonself_place_list.pop(0)
+        rbd_nonself_place_list.pop(0)
+
+        # print(nonself_num_list)
+        # print(rbd_nonself_list)
+        # print(nonself_place_list)
+        print(rbd_nonself_place_list[0])
+    
+    
+csv_nonself_count("./csv/omicron.csv","omicron")
+    
+
+            
+        
+            
